efb67d3559111c6c70658bfad6045faddb19874c
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-03-23 16:38:57 -0400

    commit

--- idade/age.js
+++ idade/age.js
@@ -1,12 +1,15 @@
+//Neste desafio, você criará uma função que nos diz
+//quantos dias, semanas e meses nos restam se vivermos até os 90 anos.
+
 function lifeInWeeks() {
 
 
     var age = parseInt(prompt("qual é a sua idade?")); //converte p num
 
-    var ageConvertida = 90 - age;
-    var ageDays = ageConvertida * 365;
-    var ageWeeks = ageConvertida * 52;
-    var ageMonths = ageConvertida * 12;
+    var ageRestante = 90 - age;
+    var ageDays = ageRestante * 365;
+    var ageWeeks = ageRestante * 52;
+    var ageMonths = ageRestante * 12;
 
 
     alert("voce tem " + ageDays + " dias, " + ageWeeks + " semanas e " + ageMonths + " meses restantes.");
