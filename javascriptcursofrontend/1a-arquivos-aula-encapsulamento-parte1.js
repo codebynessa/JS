86bd1233ab82ef7d4449299a6cfe8f527e156b04
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-06-20 23:01:23 -0400

    poo

---
+++ javascriptcursofrontend/1a-arquivos-aula-encapsulamento-parte1.js
@@ -0,0 +1,45 @@
+// Pilar - Encapsulamento
+/*
+class Carro {
+  constructor(){
+    this.modelo = "Gol"
+    this.cor = "Vermelho"
+  }
+  
+  frear(){
+    /* freio com tecnologia a disco
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    .
+    
+    console.log("parar o carro")
+  }
+  
+}
+
+const carro = new Carro()
+carro.frear()
+*/
+
+// Encapsulamento, controle de acesso e getters e setter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
