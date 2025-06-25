9590e88e001d4e14a7b3668e5987068b6c2bbea1
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-06-25 19:54:08 -0400

    arrays

---
+++ javascriptcursofrontend/1a-arquivos-aula-metodos-uteis.js
@@ -0,0 +1,25 @@
+const estados = ["São Paulo", "Ceará", "Rio de Janeiro", "Bahia", "Minas Gerais"]
+
+/*** Remove elementos do array*/
+//estados.pop()//remove último
+//estados.shift()//remove primeiro
+
+/*** Adiciona novo elemento no array*/
+//estados.push("teste")//adiciona no final
+//estados.unshift("teste")//adiciona no inicio
+
+/*** Retorna novo array */
+//const novo = estados.splice(1,1, "teste1")//emendar
+//const novo = estados.slice(2,5)//cortar
+
+/*** Converte Array / String */
+const usuarios = ["Jamilton", "Ana", "Carla"]
+let texto = usuarios.join()
+let arrayTexto = texto.split(",")
+
+console.log(arrayTexto)
+
+
+
+
+
