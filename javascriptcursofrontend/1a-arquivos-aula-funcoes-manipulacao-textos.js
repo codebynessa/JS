a727748f001c649fcfd022096e645a87c0e945b9
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-06-24 21:40:39 -0400

    manipulação

---
+++ javascriptcursofrontend/1a-arquivos-aula-funcoes-manipulacao-textos.js
@@ -0,0 +1,52 @@
+/*const nome = {
+  texto: "ja",
+  length: 2,
+  charAt(){}
+}*/
+
+
+//nome.length
+//nome.charAt(4)
+//let n = nome.replace("jamilton", "pedro")
+/*
+let nome = "carro do jamilton"
+let frase = "O sucesso é ir de fracasso em fracasso sem perder o entusiasmo"
+let n = frase.substr(0,30) + "..."
+
+console.log(n)
+let nome = "jamilton damasceno"
+let resultado = nome.split(" ")
+console.log(resultado)
+
+let nome = "pedro"
+let sobrenome = "silverio"
+let s = sobrenome.slice(0,6)
+
+nome + s + "@empresa.com.br"
+
+let pesquisa = "PEdro SILva"
+let itemSalvo = "pedro silva"
+//pesquisa.toUpperCase()
+let item = pesquisa.toLowerCase()
+if(itemSalvo == item){
+  console.log("Achei")
+}
+
+//jamiltondamasc@empresa.com.br
+
+let nome = "    Pedro Silva     "
+nome
+nome.trim()
+*/
+let a = "Olá"
+let b = "Jamilton"
+let c = "Damasceno"
+
+a.concat(" ", b, " ", c)
+
+
+
+
+
+
+
