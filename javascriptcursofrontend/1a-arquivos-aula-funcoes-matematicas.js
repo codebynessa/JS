4ae0ace5b6809554fbaa71f5d232100874ec1ae0
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-06-25 12:41:01 -0400

    op. math

---
+++ javascriptcursofrontend/1a-arquivos-aula-funcoes-matematicas.js
@@ -0,0 +1,11 @@
+let numero = 280.50
+
+//Math.PI
+//Math.ceil(numero) //arredonda para cima
+//Math.floor(numero) //arredonda para baixo
+//Math.round(numero) // arredonda
+//Math.random() //0 e 1
+//Math.min(15,10)
+//Math.max(10,20)
+//Math.pow(2,10)
+//Math.sqrt(12)
