a7e42f79e9aeecdeda1e5a3610a3e7734a712186
Vanessa Freitas <vanessafreitasferreir4@gmail.com>
2025-06-25 19:48:28 -0400

    array

---
+++ javascriptcursofrontend/1a-arquivos-aula-array-saiba-mais.js
@@ -0,0 +1,26 @@
+/*
+typeof []
+typeof new Array
+let nome = "Jamilton"
+const nomes = ["Jamilton", "Maria"]
+const frutas = new Array("Morango", "Banana")
+
+console.log(frutas[0])
+*/
+
+const nomes = [3, 1, 2]
+//nomes.push("Pedro")
+//delete nomes[2]
+//nomes[0] = "Teste"
+nomes.sort()
+console.log(nomes)
+
+
+
+
+
+
+
+
+
+
